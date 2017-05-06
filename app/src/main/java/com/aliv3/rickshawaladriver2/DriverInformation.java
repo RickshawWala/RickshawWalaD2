0d5e72246e31c99aad5d66aa2d1ff7ef535da9fb
Loel Sequeira <alenloel95@gmail.com>
2017-05-06 20:00:25 +0530

    Initial commit

---
+++ app/src/main/java/com/aliv3/rickshawaladriver2/DriverInformation.java
@@ -0,0 +1,21 @@
+package com.aliv3.rickshawaladriver2;
+
+public class DriverInformation {
+
+    public String Name;
+    public String Phone;
+    public String LicenseNo;
+    public String VehicleNo;
+
+    public DriverInformation()
+    {
+    }
+
+    public DriverInformation(String name, String phone, String licenseNo, String vehicleNo) {
+        Name = name;
+        Phone = phone;
+        LicenseNo = licenseNo;
+        VehicleNo = vehicleNo;
+
+    }
+}
---
+++ app/src/main/java/com/aliv3/rickshawaladriver2/RideActivity.java
@@ -0,0 +1,117 @@
+package com.aliv3.rickshawaladriver2;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RideActivity extends AppCompatActivity {
+
+    private final int LOCATION_REQUEST_CODE = 2; //Can be any value, non zero
+    boolean doubleBackToExitPressedOnce = false;
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride);
+
+        askPermission(android.Manifest.permission.ACCESS_FINE_LOCATION, LOCATION_REQUEST_CODE);
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.rideListView);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        Button btnBookRide = (Button) findViewById(R.id.buttonAcceptRide);
+        btnBookRide.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent iConfirmBookingActivity = new Intent(getBaseContext(), ConfirmBookingActivity.class);
+                startActivity(iConfirmBookingActivity);
+            }
+        });
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        listDataHeader.add("Text for new ride request");
+        List<String> event_1 = new ArrayList<String>();
+
+        // Adding child data
+        event_1.add("Ride request details");
+        listDataChild.put(listDataHeader.get(0), event_1); // Header, Child data
+    }
+
+    private void askPermission (String permission, int requestCode) {
+        if(ContextCompat.checkSelfPermission(this, permission)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+        } else {
+            Toast.makeText(this, "Permission is already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+}
